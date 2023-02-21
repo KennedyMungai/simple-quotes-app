f7cf13bc2ae3a8cb3a92c03cb29a52d9d46538d5
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-02-21 12:36:35 +0300

    Completed writing the routes for the various elements

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import { Route, Routes } from "react-router-dom";
+import AllQuotes from "./pages/AllQuotes";
+import NewQuote from "./pages/NewQuote";
+import QuoteDetail from "./pages/QuoteDetail";
 Quote
 function App()
 {
   return (
     <div>
       <Routes>
-        <Route to='/quotes' element={ } />
-        <Route to='/quotes/:quoteId' element={ } />
-        <Route to='/new-quote' element={ } />
+        <Route to='/quotes' element={<AllQuotes />} />
+        <Route to='/quotes/:quoteId' element={<QuoteDetail />} />
+        <Route to='/new-quote' element={<NewQuote />} />
       </Routes>
     </div>
   );
