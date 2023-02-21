e17b23f5a911a85d0689b50ec1e3ce64ca353817
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-02-21 12:32:50 +0300

    Added three routes to the App js component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import { Route, Routes } from "react-router-dom";
-
+Quote
 function App()
 {
   return (
     <div>
-
+      <Routes>
+        <Route to='/quotes' element={ } />
+        <Route to='/quotes/:quoteId' element={ } />
+        <Route to='/new-quote' element={ } />
+      </Routes>
     </div>
   );
 }
