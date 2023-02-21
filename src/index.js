2f46befeda0f9f2c197fab0e20cf965e0ff7c310
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-02-21 12:24:33 +0300

    Set up some basic routing code for the project

--- src/index.js
+++ src/index.js
@@ -3,5 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
+import React from 'react';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <React.StrictMode>
+        <Router>
+            <App />
+        </Router>
+    </React.StrictMode>
+    , document.getElementById('root'));
